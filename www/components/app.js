a7f6e56e7061b5a57f165c35bf6612799202e5a3
herikasilvaa <herikasandrade0309@gmail.com>
2022-04-05 09:16:43 +0900

    att

---
+++ www/components/app.js
@@ -0,0 +1,16 @@
+function nameAlert(){
+    var userName = document.getElementById("name").value;
+    function offuse(){
+}
+    navigator.notification.alert('Bem vindo '+userName,offuse,'Seja bem vindo','Ok');
+}
+
+function ageAlert(){
+    navigator.notification.alert('Não conseguimos fazer essa parte',offuse,'D e s c u l p a','Ok');
+}
+
+function closeApp(){
+    navigator.vibrate(3000);
+    navigator.app.exitApp();
+}
+
